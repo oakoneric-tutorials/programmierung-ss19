58d776dc22e7273d567564f6b7f16f47e137160d
eric <k-eric@outlook.com>
2019-04-12 21:14:30 +0200

    tut01 deleted templates

--- tut01/01_temp/01_02_temp.hs
+++
@@ -1,8 +0,0 @@
--- Übung Programmierung Sommersemester 2019
--- Eric Kunze
--- Github: https://github.com/oakoneric/programmierung-ss19
-------------------------------------------------------------------------
--- AUFGABE 2 --
-------------------------------------------------------------------------
-
-fib :: Int -> Int
