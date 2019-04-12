f587e0e20c4ccb263245f04a8ecfe39e5301d111
eric <k-eric@outlook.com>
2019-04-12 21:10:32 +0200

    tut01 update task 04Z

--- tut01/01_04Z_solution.hs
+++ tut01/01_04Z_solution.hs
@@ -4,10 +4,29 @@
 ------------------------------------------------------------------------
 -- AUFGABE Z1 --
 ------------------------------------------------------------------------
+-- zur Berechnung von Fibonacci-Zahlen siehe Aufgabe 2
+-- fibs verwendet die langsamere und fibs' die schnellere Variante
 
+fib :: Int -> Int
+fib 0 = 1
+fib 1 = 1
+fib n = fib (n-1) + fib (n-2)
+
+fib' :: Int -> Int
+fib' i = stack 1 1 i
+    where stack f0 f1 0 = f0
+          stack f0 f1 i = stack f1 (f0 + f1) (i-1)
+       
+       
 fibs :: [Int]
-fibs = fibs' 0
+fibs = fibAppend 0
   where
-    fibs' x = fib x : fibs' (x+1)
+    fibAppend x = fib x : fibAppend (x+1)
 
+fibs' :: [Int]
+fibs' = fibAppend 0
+  where
+    fibAppend x = fib' x : fibAppend (x+1)
+    
 -- take 7 fibs liefert die ersten 7 Fibonacci-Zahlen
+-- interessant sind insbesondere die Werte größer als 30
